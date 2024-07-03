7c63040243eba43b381dda3d20cdae44b011697b
Josiah <josiahwilliams56@gmail.com>
2024-07-02 19:32:17 -0500

    added componens to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,8 +1,37 @@
 import React from "react";
 import styles from "../css/Sidebar.module.css";
+import pfp from "../assets/pfp.jpg"
+import Button from "./Button";
+
 
 const Sidebar = () => {
-  return <div className={styles.sidebar}>Sidebar IS HERE</div>;
+  const buttons = ["Homepage", "Projects", "About", "Blog", "Contact" ];
+  return (
+    <div className={styles.sidebar}>
+      <img className={styles.pfp} src={pfp} alt="profile pic" />
+      <p className={styles.name}>Josiah Williams</p>
+      <p className={styles.title}>Aspiring IT Support Professional</p>
+      {/* <img src={ln} alt="linked incon" />
+       */}
+
+       <div className={styles.social}>
+          <div className={styles.ln_box}>
+            <i className="fa-brands fa-linkedin-in" style={{color: 'white'}}></i>
+          </div>
+
+          <div className={styles.ln_box}>
+            <i className="fa-brands fa-github" style={{color: 'white'}}></i>
+          </div>
+       </div>
+
+       <div className={styles.links}>
+         {
+          buttons.map( buttonName => <Button key={buttonName}>{buttonName}</Button>)
+         }
+       </div>
+    </div>
+  );
+  
 };
 
 export default Sidebar;
